2b7d19b1b3fcf291cee8ef19146416c27521316e
Nisha Shirawala <nisha.shirawala@intelliware.com>
2020-08-27 18:10:26 -0400

    Layout component created (chap# 124)

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Aux from '../../hoc/Aux'
+
+
+const layout = (props) => (
+    <Aux>
+        <div>
+            Toolbar, sidedrawer, backdrop
+        </div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
