00302ccfcf8fef81b2149f4c08a71e8db5115ad8
Nisha Shirawala <nisha.shirawala@intelliware.com>
2020-08-30 07:40:12 -0400

    Add Price to Order Summary dialog (#142)

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,9 +17,11 @@ const orderSummary = (props) => {
             <h3>Your Order</h3>
             <p>Burger with following ingredients</p>
             <ul>{ingredientSummary}</ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout</p>
             <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+
         </Aux>
     )
 };
