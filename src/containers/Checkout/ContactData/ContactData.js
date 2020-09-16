e9da6d3cd572b87ff1efcd2fd6afd913621c8bd4
Nisha Shirawala <nisha.shirawala@intelliware.com>
2020-09-16 14:09:37 -0400

    Handle Form Submission(Section 13 #241)

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -65,11 +65,15 @@ class ContactData extends Component {
     orderHandler = (event) => {
         event.preventDefault();
         this.setState({loading: true});
+        const formData = {};
+        for(let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-
-        }
+            orderData: formData
+        };
         axios.post('/orders.json', order)
             .then(response => {
                this.setState({loading: false});
@@ -102,7 +106,7 @@ class ContactData extends Component {
         }
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {
                     formElementArray.map(element => {
                         return <Input key = {element.id}
