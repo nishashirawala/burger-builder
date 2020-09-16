feff5e53e3a29fd1740ce3926efcb7e102ce1976
Nisha Shirawala <nisha.shirawala@intelliware.com>
2020-09-16 15:56:02 -0400

    Disable Order button (Section 13 #246 - 248)

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,9 @@ import React from 'react';
 import classes from './Button.css'
 
 const button = (props) => (
-    <button onClick={props.clicked} className={[classes.Button, classes[props.btnType]].join(' ')}>{props.children}</button>
+    <button onClick={props.clicked}
+            disabled={props.disabled}
+            className={[classes.Button, classes[props.btnType]].join(' ')}>{props.children}</button>
 );
 
 export default button;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -83,9 +83,12 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'}
                     ]
                 },
-                value: ''
+                validation: {},
+                value: 'fastest',
+                valid: true
             },
         },
+        formIsValid: false,
         loading: false
     };
 
@@ -137,8 +140,12 @@ class ContactData extends Component {
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
-        console.log(updatedFormElement);
-        this.setState({orderForm : updatedOrderForm})
+
+        let formIsValid = true;
+        for(let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+        this.setState({orderForm : updatedOrderForm, formIsValid: formIsValid});
     };
 
     render() {
@@ -166,7 +173,7 @@ class ContactData extends Component {
                     })
                 }
 
-                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                <Button btnType="Success" clicked={this.orderHandler} disabled={!this.state.formIsValid}>ORDER</Button>
             </form>
         );
         if (this.state.loading) {
