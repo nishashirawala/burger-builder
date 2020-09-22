1275ce14da51ec28b61ad3afd5b4465dd51f847d
Nisha Shirawala <nisha.shirawala@intelliware.com>
2020-09-22 14:18:29 -0400

    Using Redux to adjust order button and checkout screen (Section 15 #279-280)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,11 +26,10 @@ class BurgerBuilder extends Component {
     }
 
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
-    }
+    };
 
     updatePurchaseState = (ingredients) => {
         const sum = Object.keys(ingredients)
@@ -40,8 +39,8 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-        this.setState({purchasable: sum > 0 });
-    }
+        return sum > 0;
+    };
 
     purchaseHandler = () => {
         this.setState({purchasing:true});
@@ -52,16 +51,7 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        const queryParams = [];
-        for(let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.state.totalPrice);
-        const queryString = queryParams.join('&');
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-        });
+        this.props.history.push('/checkout');
     }
     render() {
         const disabledInfo = {
@@ -81,7 +71,7 @@ class BurgerBuilder extends Component {
                                    ingredientRemoved={this.props.onIngredientRemoved}
                                    disabled = {disabledInfo}
                                    price = {this.props.price}
-                                   purchasable = {this.state.purchasable}
+                                   purchasable = {this.updatePurchaseState(this.props.ings)}
                                    ordered = {this.purchaseHandler} />
                 </Aux>);
 
