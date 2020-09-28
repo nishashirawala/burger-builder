98c8ddef55fafd2a107d84ee710d8cbde1f73317
Nisha Shirawala <nisha.shirawala@intelliware.com>
2020-09-28 17:10:13 -0400

    Update UI based on auth state and Logout Link (Section 18 #332-333)

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,8 @@ const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
-        <NavigationItem link="/auth">Authenticate</NavigationItem>
+        {props.isAuthenticated ? <NavigationItem link="/logout">Logout</NavigationItem> : <NavigationItem link="/auth">Authenticate</NavigationItem>}
+
     </ul>
 );
 
