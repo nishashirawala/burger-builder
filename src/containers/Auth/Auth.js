5af229f66d4762b3c916310e19018cc77a2f2f80
Nisha Shirawala <nisha.shirawala@intelliware.com>
2020-09-29 09:55:58 -0400

    Redirect to checkout page (Section 18 #335)

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -42,6 +42,13 @@ class Auth extends Component {
         },
         isSignUp: true
     };
+    componentDidMount() {
+        if(!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+            this.props.onSetAuthRedirectPath();
+        }
+
+    }
+
     checkValidity(value, rules) {
         let isValid = true;
 
@@ -122,7 +129,7 @@ class Auth extends Component {
         }
         let authRedirect = null
         if (this.props.isAuthenticated) {
-            authRedirect = <Redirect to ="/"/>
+            authRedirect = <Redirect to ={this.props.authRedirectPath}/>
         }
         return (
             <div className={classes.Auth}>
@@ -145,15 +152,16 @@ const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
         error: state.auth.error,
-        isAuthenticated: state.auth.token !== null
+        isAuthenticated: state.auth.token !== null,
+        buildingBurger: state.burgerBuilder.building,
+        authRedirectPath: state.auth.authRedirectPath
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password, isSignup) => {
-            dispatch(actions.auth(email, password, isSignup))
-        }
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
+        onSetAuthRedirectPath: () => dispatch (actions.setAuthRedirectPath('/'))
     }
 };
 
