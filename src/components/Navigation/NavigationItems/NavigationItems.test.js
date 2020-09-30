d4d9cd15094a843e5b1614f2179e3b58031cc07e
Nisha Shirawala <nisha.shirawala@intelliware.com>
2020-09-30 09:48:40 -0400

    More test (Section 20 #358)

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -19,4 +19,8 @@ describe('<NavigationItems />', () => {
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
+    it('should render logout <NavigationItem /> if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
 });
