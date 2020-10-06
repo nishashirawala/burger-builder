16a5ee2c11c3f077c17966d82cea54c02d21f159
Nisha Shirawala <nisha.shirawala@intelliware.com>
2020-10-05 23:43:32 -0400

    Convert BurgerBuilder and OrderSummary to functional component  (Section 27 #460) Using React.memo for modal component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-class OrderSummary extends Component {
-    render() {
-        const ingredientSummary = Object.keys(this.props.ingredients)
-            .map(igKey => {
-                return (<li key={igKey}>
-                        <span style={{textTransform: 'capitalize'}}>
-                            {igKey}
-                        </span> : {this.props.ingredients[igKey]}
-                </li>);
-            });
+const orderSummary = props => {
 
-        return (
-            <Aux>
-                <h3>Your Order</h3>
-                <p>Burger with following ingredients</p>
-                <ul>{ingredientSummary}</ul>
-                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
-                <p>Continue to Checkout</p>
-                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
-                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (<li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>
+                        {igKey}
+                    </span> : {props.ingredients[igKey]}
+            </li>);
+        });
 
-            </Aux>
-        );
-    }
-}
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>Burger with following ingredients</p>
+            <ul>{ingredientSummary}</ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p>Continue to Checkout</p>
+            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+
+        </Aux>
+    );
+};
 
 
-export default OrderSummary;
+export default orderSummary;
